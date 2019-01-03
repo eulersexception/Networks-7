81c858b699902b66016d512be0274c3ddc3f0712
Erwin Kupris <e.kupris97@gmail.com>
2019-01-03 14:39:54 +0100

    fsmsender changed

--- FSMSender.java
+++ FSMSender.java
@@ -10,10 +10,10 @@ public class FSMSender {
     // all states for this FSM
     enum State {
         WAIT_CALL_0, WAIT_ACK_0, WAIT_CALL_1, WAIT_ACK_1
-    };
+    }
     // all messages/conditions which can occur
     enum Msg {
-        SEND_ALT_0, RCV_ACK_0, SEND_ALT_1, RCV_ACK_1
+        SEND, ALL_FINE, TIMEOUT, CORRUPT_OR_WRONG_BIT
     }
     // current state of the FSM
     private State currentState;
@@ -24,13 +24,22 @@ public class FSMSender {
      */
     public FSMSender(){
         currentState = State.WAIT_CALL_0;
-// define all valid state transitions for our state machine
-// (undefined transitions will be ignored)
         transition = new Transition[State.values().length] [Msg.values().length];
-        transition[State.WAIT_CALL_0.ordinal()] [Msg.SEND_ALT_0.ordinal()] = new SentZero();
-        transition[State.WAIT_ACK_0.ordinal()] [Msg.RCV_ACK_0.ordinal()] = new RcvZero();
-        transition[State.WAIT_CALL_1.ordinal()] [Msg.SEND_ALT_1.ordinal()] = new SentOne();
-        transition[State.WAIT_ACK_1.ordinal()] [Msg.RCV_ACK_1.ordinal()] = new RcvOne();
+
+        transition[State.WAIT_CALL_0.ordinal()] [Msg.SEND.ordinal()] = new SendPacket();
+        //transition[State.WAIT_CALL_0.ordinal()] [Msg.RCV.ordinal()] = new DoNothing();
+
+        transition[State.WAIT_ACK_0.ordinal()] [Msg.ALL_FINE.ordinal()] = new ReceiveACK();
+        transition[State.WAIT_ACK_0.ordinal()] [Msg.CORRUPT_OR_WRONG_BIT.ordinal()] = new DoNothing();
+        transition[State.WAIT_ACK_0.ordinal()] [Msg.TIMEOUT.ordinal()] = new ResendAfterTimeout();
+
+        transition[State.WAIT_CALL_1.ordinal()] [Msg.SEND.ordinal()] = new SendPacket();
+        //transition[State.WAIT_CALL_1.ordinal()] [Msg.RCV.ordinal()] = new DoNothing();
+
+        transition[State.WAIT_ACK_1.ordinal()] [Msg.ALL_FINE.ordinal()] = new ReceiveACK();
+        transition[State.WAIT_ACK_1.ordinal()] [Msg.CORRUPT_OR_WRONG_BIT.ordinal()] = new DoNothing();
+        transition[State.WAIT_ACK_1.ordinal()] [Msg.TIMEOUT.ordinal()] = new ResendAfterTimeout();
+
         System.out.println("INFO FSM constructed, current state: "+currentState);
     }
     /**
@@ -53,36 +62,62 @@ public class FSMSender {
     abstract class Transition {
         abstract public State execute(Msg input);
     }
-    class SentZero extends Transition {
+    class SendPacket extends Transition {
         @Override
         public State execute(Msg input) {
-            // FileSender methods for sending packet with bit 0
-            System.out.println("Hi!");
-            return State.WAIT_ACK_0;
+            int bitNumber = 0;
+            if(currentState == State.WAIT_CALL_0) {
+                System.out.println("Packet with bit 0 sent, wait for ACK 0");
+                currentState = State.WAIT_ACK_0;
+            }
+            else {
+                System.out.println("Packet sent, wait for ACK 1");
+                bitNumber = 1;
+                currentState = State.WAIT_ACK_1;
+            }
+
+            // send(bitNumber)
+            return currentState;
         }
     }
-    class RcvZero extends Transition {
+
+    class ReceiveACK extends Transition {
         @Override
         public State execute(Msg input) {
-            // FileSender methods after receiving a packet with ack 0
-            System.out.println("Time?");
-            return State.WAIT_CALL_1;
+            if(currentState == State.WAIT_ACK_0) {
+                System.out.println("ACK 0 received.");
+                currentState = State.WAIT_CALL_0;
+            }
+            else {
+                System.out.println("ACK 1 received.");
+                currentState = State.WAIT_CALL_1;
+            }
+            return currentState;
+        }
+        // stoptimer();
+    }
+
+    class DoNothing extends Transition {
+        @Override
+        public State execute(Msg input) {
+            System.out.println("ACK was received, but is either corrupt or has wrong bit.");
+            return currentState;
         }
     }
-    class SentOne extends Transition {
+
+    class ResendAfterTimeout extends Transition {
         @Override
         public State execute(Msg input) {
-            // FileSender methods for sending packet with bit 1
-            System.out.println("Thank you.");
-            return State.WAIT_CALL_0;
-        }
-    }
-    class RcvOne extends Transition {
-        @Override
-        public State execute(Msg input) {
-            // FileSender methods after receiving a packet with ack 0
-            System.out.println("Thank you.");
-            return State.WAIT_CALL_0;
+            int bitNumber = 0;
+            if(currentState == State.WAIT_ACK_0) {
+                System.out.println("Timeout occured: Resend packet 0.");
+            }
+            else {
+                System.out.println("Timeout occured: Resend packet 1.");
+                bitNumber = 1;
+            }
+            // resend(bitNumber)
+            return currentState;
         }
     }
 }
